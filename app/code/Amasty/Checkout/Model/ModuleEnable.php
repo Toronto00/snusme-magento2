d6c3eee5a071d24261a4bfae6e7fdd92abada852
Jacob Holm <jacob.holm@webbhuset.se>
2018-06-10 10:07:05 +0000

    Installed Amasty OneStepCheckout

---
+++ app/code/Amasty/Checkout/Model/ModuleEnable.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @author Amasty Team
+ * @copyright Copyright (c) 2018 Amasty (https://www.amasty.com)
+ * @package Amasty_Checkout
+ */
+
+
+namespace Amasty\Checkout\Model;
+
+class ModuleEnable
+{
+    const TIG_POSTNL_MODULE_NAMESPACE = 'TIG_PostNL';
+
+    const AMASTY_STOCKSTATUS_MODULE_NAMESPACE = 'Amasty_Stockstatus';
+
+    const AMASTY_REWARD_POINTS_MODULE_NAMESPACE = 'Amasty_Rewards';
+
+    /**
+     * @var \Magento\Framework\Module\Manager
+     */
+    private $moduleManager;
+
+    public function __construct(\Magento\Framework\Module\Manager $moduleManager)
+    {
+        $this->moduleManager = $moduleManager;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isPostNlEnable()
+    {
+        return $this->moduleManager->isEnabled(self::TIG_POSTNL_MODULE_NAMESPACE);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isStockStatusEnable()
+    {
+        return $this->moduleManager->isEnabled(self::AMASTY_STOCKSTATUS_MODULE_NAMESPACE);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRewardsPointEnable()
+    {
+        return $this->moduleManager->isEnabled(self::AMASTY_REWARD_POINTS_MODULE_NAMESPACE);
+    }
+}
