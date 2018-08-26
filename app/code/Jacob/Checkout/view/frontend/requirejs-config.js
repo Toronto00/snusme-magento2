689efb4d7a3586ebc1e54a285ffe1d996c2554b6
Jacob Holm <jacob@webbhuset.se>
2018-08-26 09:40:49 +0000

    Qty spinners in cart, frontpage usps, corrections to product list, added my account

---
+++ app/code/Jacob/Checkout/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Checkout/template/minicart/item/default.html':
+                'Jacob_Checkout/template/minicart/item/default.html'
+        }
+    }
+};
