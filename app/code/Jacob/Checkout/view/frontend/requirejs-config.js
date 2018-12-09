dd3c56489cf0743e96b31d453a16fd01cc614442
Jacob Holm <jacob@webbhuset.se>
2018-12-09 16:59:44 +0000

    Added route to root category

--- app/code/Jacob/Checkout/view/frontend/requirejs-config.js
+++ app/code/Jacob/Checkout/view/frontend/requirejs-config.js
@@ -2,7 +2,9 @@ var config = {
     map: {
         '*': {
             'Magento_Checkout/template/minicart/item/default.html':
-                'Jacob_Checkout/template/minicart/item/default.html'
+                'Jacob_Checkout/template/minicart/item/default.html',
+            'Magento_Checkout/template/minicart/content.html':
+                'Jacob_Checkout/template/minicart/content.html',
         }
     }
 };
